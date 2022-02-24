6bedf69e0e7f01f517d8e82a68ea62783a4ad5a0
BaoThuong96 <baothuong9608@gmail.com>
2022-02-24 20:27:34 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import LoginPage10 from './LoginPage10';
+import LoginPage2 from './LoginPage2';
+import LoginPage3 from './LoginPage3';
+import LoginPage4 from './LoginPage4';
+import LoginPage5 from './LoginPage5';
+import LoginPage6 from './LoginPage6';
+import LoginPage7 from './LoginPage7';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			{/* <LoginPage2 /> */}
+			{/* <LoginPage3 /> */}
+			{/* <LoginPage4 /> */}
+			{/* <LoginPage5 /> */}
+			{/* <LoginPage6 /> */}
+			{/* <LoginPage7 /> */}
+			<LoginPage10 />
+		</div>
+	);
 }
 
 export default App;
